b6564f6be500361767c09fd138221bfea621f3b7
hannahpan1 <hannahpan10@gmail.com>
2024-08-07 11:26:19 -0400

    More sfx

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -6,11 +6,18 @@ public class Teleporter : MonoBehaviour
 {
     public Transform player, destination, dog;
     public GameObject playerg;
+    private AudioSource _audioSource;
+    
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            _audioSource.Play();
             playerg.SetActive(false);
             player.position = destination.position;
             dog.position = destination.position;
